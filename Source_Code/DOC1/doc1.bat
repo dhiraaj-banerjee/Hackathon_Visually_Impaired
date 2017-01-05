59ad33af07550a07ae0afa104f612b98e71b73ea
Dhiraaj Banerjee <dhiraaj.banerjee@pb.com>
2017-01-05 15:40:06 +0530

    Revert "1st Version"
    This reverts commit 134017664a16b7bc78f4c3b990d03aa7918657c1.

--- Source_Code/DOC1/doc1.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\PBBI CCM\DOC1\Designer\Client\doc1gen.exe" Braille_AFP.hip
-pause
--- Source_Code/Spectrum/spectrum.bat
+++
@@ -1,5 +0,0 @@
-
-
-java -jar "C:\Program Files\Pitney Bowes\Spectrum\server\app\exports\jobexecutor.jar" -s 9797 -j Hackathon_demo -u admin -p "admin" -w -r>>"E:\Hackathon\Source_Code\Spectrum\log.txt"  "Read from File"="file:E:\Hackathon\Source_Code\Spectrum\CustomerDetails.csv"  "Read from File 2"="file:E:\Hackathon\Source_Code\Spectrum\AccountDetails.csv" "Write to File"="file:E:\Hackathon\Source_Code\Spectrum\Data.txt"
-pause;
-
